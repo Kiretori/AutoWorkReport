6ba72b35e0f814f899c02e7396be5527cb07b426
Kiretori <kiretori2003@gmail.com>
2025-08-01 16:15:36 +0100

    Refactor daily and weekly report flows: update flow names to include dynamic date generation and improve date handling logic.

--- flows/weekly_report.py
+++ flows/weekly_report.py
@@ -1,17 +1,32 @@
+from typing import Tuple
 from prefect import flow
 from prefect.logging import get_run_logger
-from datetime import date
+from datetime import date, timedelta
 from tasks.utils import fetch_weekly_data, generate_weekly_excel
 from email_service.email_generator import generate_weekly_report_html
 from email_service.email_sender import send_daily_email
 from database.models import EmailData
 
 
-@flow
+def get_last_workweek() -> Tuple[date, date]:
+    today = date.today()
+    last_monday = today - timedelta(days=today.weekday() + 7)
+    last_friday = last_monday + timedelta(days=4)
+    return last_monday, last_friday
+
+
+def generate_weekly_flow_name() -> str:
+    start, end = get_last_workweek()
+    start = start.strftime("%Y%m%d")
+    end = end.strftime("%Y%m%d")
+    return f"weekly-report-{start}-{end}"
+
+
+@flow(flow_run_name=generate_weekly_flow_name)
 def weekly_report():
     logger = get_run_logger()
 
-    start_date, end_date = date(2025, 7, 7), date(2025, 7, 11)
+    start_date, end_date = get_last_workweek()
     weekly_data = fetch_weekly_data(start_date, end_date)
     logger.info("Generating excel sheets")
     xlsx_filepath = generate_weekly_excel(start_date, end_date, "weekly_report")
